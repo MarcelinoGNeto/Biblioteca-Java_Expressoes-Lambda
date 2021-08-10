d8eebc5129d85c1c71a2965afcf89d8dea256ddc
Marcelino Neto <marcelino.gneto@gmail.com>
2021-08-10 18:29:59 -0300

    Consumer: implementacao pela interface - classe

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,33 +2,46 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Locale;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+
+//---------- CONSUMER ----------------
+		list.forEach(new PriceUpdate());
 		
-//		list.removeIf(new ProductPredicate()); //Referecia a expressao lambda de uma classe
-//		list.removeIf(Product::staticProductPredicate); //Referencia a expressão lambda do método estático de uma classe
-//		list.removeIf(Product::nonStaticProductPredicate); //Referencia a expressão lambda do método não estático de uma classe
+		list.forEach(System.out::println);
+		
+		
+	
+// --------- PREDICATE ----------------		
 /*
+		list.removeIf(new ProductPredicate()); //Referecia a expressao lambda de uma classe
+		list.removeIf(Product::staticProductPredicate); //Referencia a expressão lambda do método estático de uma classe
+		list.removeIf(Product::nonStaticProductPredicate); //Referencia a expressão lambda do método não estático de uma classe
+
 		Predicate<Product> pred = p -> p.getPrice() >= 100.0; //expressão lambda declarada
 		list.removeIf(pred); //complemento da implementação da expressão lambda declarada
-*/
+
 		list.removeIf(p -> p.getPrice() >= 100.0); //in line - expressão lambda completa declarada na mesma linha
 		
 		for (Product p : list) {
 			System.out.println(p);
-		}
 
+		}
+*/
+		
 	}
 }
